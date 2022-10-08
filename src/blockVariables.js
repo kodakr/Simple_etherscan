8cac48e21e232c03f5128fb3722f9a0c4b0397c8
kodakr <111961380+kodakr@users.noreply.github.com>
2022-10-08 13:27:17 +0200

    Update blockVariables.js
    removal of infuraID

--- src/blockVariables.js
+++ src/blockVariables.js
@@ -1,6 +1,6 @@
 // import Ethers from "ethers";
 
-// const infuraid = "3d05647a39544dafab60d295c1ece741";
+// const infuraid = "";
 // const provider = new Ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${infuraid}`);
 // let EthBalance;
 // //const bal = await provider.getBalance(address)
