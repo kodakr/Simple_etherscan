4b728e968317145760eaa1ae01a862f7d0a5de5c
kodakr <111961380+kodakr@users.noreply.github.com>
2022-10-27 03:19:35 +0200

    Update test.js
    update id

--- src/test.js
+++ src/test.js
@@ -2,7 +2,7 @@
 
 // const { ethers } = require("ethers");
 // //import ethers from 'ethers';
-// const infuraid = "3d05647a39544dafab60d295c1ece741";
+// const infuraid = "3d05gftrt64nj7a395477t4daf29mk5c1eckie74";
 // const setup = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${infuraid}`);
 // const address = '0x4dd73247f6b7232a8fdea1f3b00cdd8c1de79b8f';
 
