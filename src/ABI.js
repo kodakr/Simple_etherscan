9288a1347045f66a4ba06ff87702cfafbc1f4a22
kodakr <111961380+kodakr@users.noreply.github.com>
2022-10-27 03:21:36 +0200

    Create ABI.js

---
+++ src/ABI.js
@@ -0,0 +1,8 @@
+const ERC20_ABI = [
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function totalSupply() view returns (uint256)",
+    "function balanceOf(address) view returns (uint)",
+];
+
+module.exports = { ERC20_ABI};
